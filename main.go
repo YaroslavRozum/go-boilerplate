46757be04a59fa149ae4fe8224fe73e1a9c3ed71
Yaroslav Rozum <flashglbm@gmail.com>
2019-09-25 11:00:49 +0300

    removed unnecessary code

--- main.go
+++ main.go
@@ -8,15 +8,3 @@ func main() {
 	r := createRouter()
 	http.ListenAndServe(":3000", r)
 }
-
-type L struct {
-	name string
-}
-
-func (l *L) setName(name string) {
-	l.name = name
-}
-
-type Xl struct {
-	L *L
-}
